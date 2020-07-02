59506f39a6d4a022d0ed85725ba0970a4df7cb81
David Eads <davideads@gmail.com>
2020-07-01 23:57:09 -0500

    update homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,11 +8,19 @@ const IndexPage = () => (
     <div className="warning">Under active development, do not share.</div>
     <Layout>
       <div className="title">
-        <h1>Chicago Police Department Arrest API</h1>
+        <h1>Illinois COVID-19 data API</h1>
         <p>David Eads, The Chicago Reporter</p>
       </div>
       
       <p><Link to="explorer">Explore the data using the interactive console</Link></p>
+
+      <p>This is a currently experimental collection of COVID-19 related datasets focused on local data in Illinois. These datasets provide an anonymous, read-only GraphQL API built on Hasura. Hasura exposes a number of standard, SQL-like query features that <a href="https://hasura.io/docs/1.0/graphql/manual/queries/index.html">you can learn about in the Hasura docs</a>.</p>
+
+      <p>Current datasets hosted:</p>
+
+      <ul>
+        <li>IDPH historical ZIP code data: Contains overall cases, test counts, and well as cases and test counts for race, age, and gender demographic groups. April 15 - June 17, 2020.</li>
+      </ul>
     </Layout>
   </>
 )
